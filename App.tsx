8e324b449e770dc4be9a89b4b9af24f3a4c92ac6
Turtle <gustavsson.sofia@hotmail.com>
2024-09-24 22:26:46 +0200

    add mocked data + display it + a function to show the image

--- App.tsx
+++ App.tsx
@@ -1,14 +1,42 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { MockFriendsData } from "./data";
 
 const RootStack = createNativeStackNavigator();
+
+function resolveImage(imageName: string) {
+  switch (imageName) {
+    case "pexels-javon-swaby-197616-1697912.jpg":
+      return require("./assets/images/pexels-javon-swaby-197616-1697912.jpg");
+    case "pexels-luizclas-170497-1848565.jpg":
+      return require("./assets/images/pexels-luizclas-170497-1848565.jpg");
+    case "pexels-olly-837358.jpg":
+      return require("./assets/images/pexels-olly-837358.jpg");
+    case "pexels-trinitykubassek-445109.jpg":
+      return require("./assets/images/pexels-trinitykubassek-445109.jpg");
+    default:
+      return require("./assets/images/default.jpg"); // fallback image if needed
+  }
+}
 
 function HomeScreen() {
   return (
     <View>
       <Text>Home Screen 🐛</Text>
+      {MockFriendsData.map((friend) => (
+        <View>
+          <Text key={friend.id}>
+            {friend.name} is my {friend.relation}
+          </Text>
+
+          <Image
+            source={resolveImage(friend.image)}
+            style={{ width: 100, height: 100 }}
+          />
+        </View>
+      ))}
     </View>
   );
 }

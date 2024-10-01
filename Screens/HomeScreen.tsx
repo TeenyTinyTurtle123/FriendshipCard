63d75a443f60a9332d3354dcfd197b9049eb8d0f
Turtle <gustavsson.sofia@hotmail.com>
2024-10-01 15:21:48 +0200

    add remove and add friend function to FriendProvider + testing it out on home screen

--- Screens/HomeScreen.tsx
+++ Screens/HomeScreen.tsx
@@ -1,12 +1,47 @@
-import { Button, View } from "react-native";
+import { Button, Text, View } from "react-native";
+import { useFriendProvider } from "../components/FriendProvider";
+import { FriendCard } from "../data";
 
 export default function HomeScreen({ navigation }: any) {
+  const { friendList, addFriend, removeFriend } = useFriendProvider();
+
+  const handleAddAFriend = () => {
+    const newFriend: FriendCard = {
+      id: Date.now(), // Generate a unique ID
+      name: "Sunny Andersson",
+      relation: "Acquaintance",
+      likes: ["Music", "Sports"],
+      giftIdea: ["Concert tickets"],
+      image: "",
+    };
+
+    addFriend(newFriend);
+  };
+
+  const handleRemoveFriend = (id: number) => {
+    removeFriend(id);
+  };
+
   return (
     <View>
       <Button
         title="Go to Display ❤️"
         onPress={() => navigation.navigate("Display")}
       ></Button>
+
+      <Text>Friends: </Text>
+      {friendList.map((friend) => (
+        <View key={friend.id}>
+          <Text>
+            {friend.name} {friend.relation} {friend.likes}
+          </Text>
+          <Button
+            title="Remove friend"
+            onPress={() => handleRemoveFriend(friend.id)}
+          />
+        </View>
+      ))}
+      <Button title="Add new friend" onPress={handleAddAFriend} />
     </View>
   );
 }
--- components/FriendProvider.tsx
+++ components/FriendProvider.tsx
@@ -1,20 +1,38 @@
-import { createContext, PropsWithChildren } from "react";
+import { createContext, PropsWithChildren, useContext } from "react";
 import { FriendCard } from "../data";
 import useAsyncStore from "../Hooks/useAsyncStorage";
 
 interface ContextValue {
   friendList: FriendCard[];
+  addFriend: (friend: FriendCard) => void;
+  removeFriend: (id: number) => void;
 }
 
 export const FriendContext = createContext<ContextValue>({} as ContextValue);
 
 export default function FriendProvider({ children }: PropsWithChildren) {
   // Async Storage saker
-  const [friends, setFriends] = useAsyncStore("friends", []);
+  const [friends, setFriends] = useAsyncStore<FriendCard[]>("friends", []);
+
+  // uppdaterar listan med ett nytt moment
+  const addFriend = (newFriend: FriendCard) => {
+    const updateFriends = [...friends, newFriend];
+    setFriends(updateFriends);
+  };
+
+  // uppdaterar listan med alla saker FÖRUTOM objektet med inkommande id
+  const removeFriend = (id: number) => {
+    const updateFriends = friends.filter((friend) => friend.id !== id);
+    setFriends(updateFriends);
+  };
 
   return (
-    <FriendContext.Provider value={{ friendList: friends }}>
+    <FriendContext.Provider
+      value={{ friendList: friends, addFriend, removeFriend }}
+    >
       {children}
     </FriendContext.Provider>
   );
 }
+
+export const useFriendProvider = () => useContext(FriendContext);
